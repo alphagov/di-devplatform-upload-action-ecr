634398f7d046708ce7ab07b74df601c3bf9baf44
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-08-31 15:22:06 +0100

    Initial commit - action and script to tag/upload/replace ref
    An action which takes a number of inputs to specify which bucket,
    role and ECR repo to push to as well as a KMS key, and the
    working directory of the containers to be pushed.
    Also a script which does the actual building and tagging then
    replaces the references in the template file and uploads it to
    the artifact bucket(s).

---
+++ scripts/build-tag-push-ecr.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -eu
+
+echo "building image(s)"
+#WORKING_DIRECTORY_1="1"
+#WORKING_DIRECTORY_2="2"
+#WORKING_DIRECTORY_3="3"
+#WORKING_DIRECTORY_4="none"
+count=1
+while [ $count -le 4 ]
+do
+  working_dir="WORKING_DIRECTORY_$count"
+  ecr_repo_name="ECR_REPO_NAME_$count"
+  artifact_bucket_name="ARTIFACT_BUCKET_NAME_$count"
+  if [ "${!working_dir}" != "none" ]; then
+    echo "Packaging app from ${!working_dir}"
+    docker build -t $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA .
+    docker push $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA
+    cosign sign --key awskms:///${CONTAINER_SIGN_KMS_KEY_ARN} $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA
+
+    sam package --s3-bucket="$artifact_bucket_name" --output-template-file=cf-template.yaml
+
+    sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA|" cf-template.yaml
+    zip template.zip cf-template.yaml
+    aws s3 cp template.zip "s3://$artifact_bucket_name/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
+  fi
+  count=$((count+1))
+done
