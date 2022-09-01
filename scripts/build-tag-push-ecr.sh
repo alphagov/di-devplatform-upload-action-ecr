505d4f66b0faca8277fdf7b2d53476ce7967e178
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-09-01 17:00:03 +0100

    remove code dealing with >1 container per repo
    remove the code which dealt with more than one container as
    out of scope for this

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -1,32 +1,18 @@
 #! /bin/bash
 
-set -e
+set -eu
 
 echo "building image(s)"
-echo 'current dir =' $(pwd)
-firstdir=$(pwd)
-count=1
-while [ $count -le 3 ]
-do
-  cd "${firstdir}"
-  working_dir="WORKING_DIRECTORY_$count"
-  ecr_repo_name="ECR_REPO_NAME_$count"
-  artifact_bucket_name="ARTIFACT_BUCKET_NAME_$count"
-  template_file="TEMPLATE_FILE_$count"
 
-  if [ "${!working_dir}" != "none" ]; then
-    cd ${!working_dir}
-    echo "Packaging app from ${!working_dir}"
+cd "$WORKING_DIR"
+echo "Packaging app in /$WORKING_DIR"
 
-    docker build -t $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA .
-    docker push $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA
-    cosign sign --key awskms:///${CONTAINER_SIGN_KMS_KEY_ARN} $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA
+docker build -t "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA" .
+docker push "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
+cosign sign --key "awskms:///${CONTAINER_SIGN_KMS_KEY_ARN}" "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
 
-    sam package --template-file="${!template_file}" --s3-bucket="${!artifact_bucket_name}" --output-template-file=cf-template.yaml
+sam package --template-file="$TEMPLATE_FILE" --s3-bucket="$ARTIFACT_BUCKET_NAME" --output-template-file=cf-template.yaml
 
-    sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA|" cf-template.yaml
-    zip template.zip cf-template.yaml
-    aws s3 cp template.zip "s3://${!artifact_bucket_name}/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
-  fi
-  count=$((count+1))
-done
+sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA|" cf-template.yaml
+zip template.zip cf-template.yaml
+aws s3 cp template.zip "s3://$ARTIFACT_BUCKET_NAME/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
