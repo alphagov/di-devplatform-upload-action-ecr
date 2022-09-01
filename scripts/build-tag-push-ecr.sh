08bfbb308a3545ee73642d41fba99aca00b2eaa3
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-09-01 09:49:43 +0100

    correct expansion of variables
    expand out the variable names for the repo and bucket

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -15,15 +15,15 @@ do
   artifact_bucket_name="ARTIFACT_BUCKET_NAME_$count"
   if [ "${!working_dir}" != "none" ]; then
     echo "Packaging app from ${!working_dir}"
-    docker build -t $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA .
-    docker push $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA
-    cosign sign --key awskms:///${CONTAINER_SIGN_KMS_KEY_ARN} $ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA
+    docker build -t $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA .
+    docker push $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA
+    cosign sign --key awskms:///${CONTAINER_SIGN_KMS_KEY_ARN} $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA
 
-    sam package --s3-bucket="$artifact_bucket_name" --output-template-file=cf-template.yaml
+    sam package --s3-bucket="${!artifact_bucket_name}" --output-template-file=cf-template.yaml
 
-    sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/$ecr_repo_name:$GITHUB_SHA|" cf-template.yaml
+    sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA|" cf-template.yaml
     zip template.zip cf-template.yaml
-    aws s3 cp template.zip "s3://$artifact_bucket_name/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
+    aws s3 cp template.zip "s3://${!artifact_bucket_name}/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
   fi
   count=$((count+1))
 done
