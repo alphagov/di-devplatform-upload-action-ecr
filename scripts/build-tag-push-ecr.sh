784b78c5bd4213223c7b3cb8b40b9dc51f62259c
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-09-01 10:43:15 +0100

    Make sure working dir is not empty
    Add condition that the working directory is not empty - may
    change this later because it should be allowed

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "building image(s)"
 echo 'current dir =' $(pwd)
-initialdir=$(pwd)
+firstdir=$(pwd)
 #WORKING_DIRECTORY_1="1"
 #WORKING_DIRECTORY_2="2"
 #WORKING_DIRECTORY_3="3"
@@ -12,11 +12,11 @@ initialdir=$(pwd)
 count=1
 while [ $count -le 4 ]
 do
-  cd "${initialdir}"
+  cd "${firstdir}"
   working_dir="WORKING_DIRECTORY_$count"
   ecr_repo_name="ECR_REPO_NAME_$count"
   artifact_bucket_name="ARTIFACT_BUCKET_NAME_$count"
-  if [ "${!working_dir}" != "none" ]; then
+  if [ "${!working_dir}" != "none" ] && [ "${!working_dir}" != "" ]; then
     echo "Packaging app from ${!working_dir}"
     cd ${!working_dir}
     docker build -t $ECR_REGISTRY/${!ecr_repo_name}:$GITHUB_SHA .
