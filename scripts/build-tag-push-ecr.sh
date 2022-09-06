4444ccc65f6e34b6a51442a4c0179b4f2b7dc73c
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2022-09-06 16:52:33 +0100

    Added output and check for placeholder text
    Put in place more output and a warning if the placeholder
    text is not present in the template file

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -11,9 +11,15 @@ docker build -t "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA" .
 docker push "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
 cosign sign --key "awskms:///${CONTAINER_SIGN_KMS_KEY_ARN}" "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
 
+echo "Running sam build on template file"
 sam build --template-file="$TEMPLATE_FILE"
 mv .aws-sam/build/template.yaml cf-template.yaml
 
-sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA|" cf-template.yaml
+if grep -q "CONTAINER-IMAGE-PLACEHOLDER"; then
+  echo "Replacing \"CONTAINER-IMAGE-PLACEHOLDER\" with new ECR image ref"
+  sed -i "s|CONTAINER-IMAGE-PLACEHOLDER|$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA|" cf-template.yaml
+else
+  echo "WARNING!!! Image placeholder text \"CONTAINER-IMAGE-PLACEHOLDER\" not found - uploading template anyway"
+fi
 zip template.zip cf-template.yaml
 aws s3 cp template.zip "s3://$ARTIFACT_BUCKET_NAME/template.zip" --metadata "repository=$GITHUB_REPOSITORY,commitsha=$GITHUB_SHA"
